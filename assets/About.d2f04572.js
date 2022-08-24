40c3178a4e5915df3a44f5a131227021676a01dd
hykilpikonna <hykilpikonna@users.noreply.github.com>
2022-08-24 16:34:38 +0000

    Deploying to gh-pages from @ one-among-us/web@38bd1c37271e8b0cff891543cc510ab753a4fa6a 🚀

---
+++ assets/About.d2f04572.js
@@ -0,0 +1,7 @@
+import{_ as i,c as l,a as c,V as p,O as u,o as h}from"./index.628a166a.js";var _=`<h1 id="\u8054\u7CFB\u6211\u4EEC">\u8054\u7CFB\u6211\u4EEC</h1>
+<p>\u8FD9\u4E2A\u7F51\u9875\u76EE\u524D\u662F\u7531 <a href="https://twitter.com/sauricat/">Shu Lin</a> \u548C <a href="https://github.com/hykilpikonna">Hykilpikonna</a> \u5171\u540C\u7EF4\u62A4\uFF0C\u5982\u6709\u9700\u8981\u53EF\u4EE5\u901A\u8FC7\u90AE\u4EF6\u8054\u7CFB\u6211\u4EEC\uFF01\u90AE\u7BB1\u5730\u5740\u5982\u4E0B\uFF1A</p>
+<ul>
+<li>Shu: <a href="mailto:&#108;&#105;&#x6e;&#115;&#x68;&#117;&#x31;&#x37;&#50;&#x39;&#64;&#x70;&#x72;&#111;&#x74;&#111;&#x6e;&#109;&#x61;&#105;&#x6c;&#x2e;&#99;&#x6f;&#109;">&#108;&#105;&#x6e;&#115;&#x68;&#117;&#x31;&#x37;&#50;&#x39;&#64;&#x70;&#x72;&#111;&#x74;&#111;&#x6e;&#109;&#x61;&#105;&#x6c;&#x2e;&#99;&#x6f;&#109;</a></li>
+<li>Hykilpikonna: <a href="mailto:&#x6d;&#x65;&#x40;&#x68;&#x79;&#100;&#101;&#x76;&#46;&#111;&#x72;&#x67;">&#x6d;&#x65;&#x40;&#x68;&#x79;&#100;&#101;&#x76;&#46;&#111;&#x72;&#x67;</a></li>
+</ul>
+`,f=Object.defineProperty,m=Object.getOwnPropertyDescriptor,d=(n,t,a,x)=>{for(var e=x>1?void 0:x?m(t,a):t,r=n.length-1,o;r>=0;r--)(o=n[r])&&(e=(x?o(t,a,e):o(e))||e);return x&&e&&f(t,a,e),e};let s=class extends p{constructor(){super(...arguments),this.html=_}};s=d([u({})],s);const v=s,b={id:"About"},w=["innerHTML"];function O(n,t,a,x,e,r){return h(),l("div",b,[c("div",{class:"markdown-content",style:{margin:"10px min(4vw, 40px)"},innerHTML:n.html},null,8,w)])}var H=i(v,[["render",O]]);export{H as default};
